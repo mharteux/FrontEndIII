140dd91a40e7c5f4c3a34368db4d5448bdbc9bbd
Matheus Reynaud <matheuxreynaud@gmail.com>
2024-03-29 22:49:41 -0300

    aula03

---
+++ aula03/exerc03/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aluno from './components/Aluno';
+
+const App = () => {
+  const alunos = [
+    { nome: 'Matheus', email: 'matheus@email.com', curso: 'Sistemas para Internet', media: 8.5 },
+    { nome: 'Júlia', email: 'julia@email.com', curso: 'Medicina', media: 6.8 },
+    { nome: 'Aria', email: 'aria@email.com', curso: 'Nutrição', media: 7.2 }
+  ];
+
+  return (
+    <div>
+      <h1>Lista de Alunos</h1>
+      {alunos.map((aluno, index) => (
+        <Aluno key={index} {...aluno} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
