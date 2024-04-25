5a82391f5ceb06ef532a312f3a1395d2eb7070a6
Matheus Reynaud Soares <matheuxreynaud@gmail.com>
2024-04-24 21:01:54 -0300

    aula05

---
+++ aula05/exerc01/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import gerenciamentoAlunos from './components/gerenciamentoAlunos'
+
+const App = () => {
+  return (
+    <div><gerenciamentoAlunos /></div>
+  )
+}
+
+export default gerenciamentoAlunos
